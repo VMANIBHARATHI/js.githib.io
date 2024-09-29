018bf222939bbd5cb5d86900c243a24006124417
MANIBHARATHI V <Vmanibharathi34@gmail.com>
2024-09-29 22:30:32 +0530

    upload

---
+++ controlstatements.js
@@ -0,0 +1,45 @@
+// If statement
+
+const num = 5;
+ 
+if (num > 0) {
+    console.log("The number is positive.");
+};
+
+// if else statement
+
+let  b= 10;
+
+if (b > 0)
+	console.log("true");
+else
+	console.log("falsee");
+
+// switch statement
+let a = 5;
+ 
+switch (a) {
+    case 0:
+        console.log("Number is 0.");
+        break;
+    case 1:
+        console.log("Nuber is 1.");
+        break;
+    case 2:
+        console.log("Number is 2.");
+        break;
+    default:
+        console.log("Number is greater than 2.");
+};
+
+// else if statement
+
+let c = 0;
+
+if (c > 0) {
+    console.log("c is positive.");
+} else if (c < 0) {
+    console.log("c is negative.");
+} else {
+    console.log("c is zero.");
+};
