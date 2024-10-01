850da6054f8f2a5a4f328b4df16ecf8af8bdf489
MANIBHARATHI V <Vmanibharathi34@gmail.com>
2024-10-01 22:02:14 +0530

    upload

---
+++ EC.js
@@ -0,0 +1,16 @@
+var n = 5;
+
+function square(n) {
+  var ans = n * n;
+  return ans;
+}
+
+var square1 = square(n);
+var square2 = square(8);  
+
+console.log(square1)
+console.log(square2)
+
+// Execution Context-------------
+// Creation phase--------
+  GlobalExecuitnContext
