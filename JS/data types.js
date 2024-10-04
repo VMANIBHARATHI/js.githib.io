d854bddc9ae238f3f2a8ae9993a096cb0f5636be
MANIBHARATHI <144135234+VMANIBHARATHI@users.noreply.github.com>
2024-10-04 07:45:07 +0530

    Update data types.js

---
+++ JS/data types.js
@@ -0,0 +1,46 @@
+// Primitive  Data types
+// numbers
+var a = 16;
+var b = 7.5;
+console.log(a);
+console.log( b);
+
+// strings
+var firstname = "Mani";
+var lastName = "bharathi";
+console.log("Value of firstnmae=" + firstname);
+console.log("Value of lastname=" + lastName);
+
+// boolean
+var x = true;
+var y = false;
+
+// undefined
+let x;
+console.log(x);
+
+// null
+let x = null;
+    console.log(x);
+
+// Boolean
+
+let x;
+console.log(x);
+
+// Non primite data types
+
+// object 
+
+let student = {
+    firstName: "MANI",
+    lastName: "BHARATHI",
+}
+
+console.log(student.firstName + " " + student.lastName);
+
+// array
+
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers[0]); 
+console.log(numbers[2]);
