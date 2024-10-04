ab0abc8cbe3b3828caae5ddc919aa166bf3f6d12
MANIBHARATHI <144135234+VMANIBHARATHI@users.noreply.github.com>
2024-10-04 07:43:25 +0530

    Update Hoisting.js

---
+++ JS/Hoisting.js
@@ -0,0 +1,14 @@
+// use test variable before declaring
+
+console.log(test);
+
+var test = 5;
+
+// Output: undefined
+
+// use the message variable before declaration
+console.log(message);
+
+var message;
+
+// Output: undefined
