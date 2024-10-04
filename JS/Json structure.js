8d169283519a7ad67fcbe7314b935de2a86cf621
MANIBHARATHI <144135234+VMANIBHARATHI@users.noreply.github.com>
2024-10-04 07:43:54 +0530

    Update Json structure.js

---
+++ JS/Json structure.js
@@ -0,0 +1,18 @@
+const Details = {
+    "name": "MAnibharathi",
+    "age":22,
+    "skills": ["JavaScript", "HTML", "CSS"],
+    "address": {
+      "street": "123 Main St",
+      "city": "Chennai",
+      "state": "TAMILNADU"
+    }
+  };
+  
+ 
+  
+  console.log(person.name);        
+  console.log(person.age);         
+  console.log(person.isEmployed);  
+  console.log(person.skills);      
+  console.log(person.address.city);
